1c83c5b9b45358b7295b4ac1a7c1efddc1dd5ffa
MichaelDB1 <michaelbarr@hotmail.co.nz>
2016-09-15 16:16:08 +1200

    ssss

--- js/mainScript.js
+++ js/mainScript.js
@@ -60,7 +60,59 @@ function translateText(message, response) {
         });
 */
 
+function speak(messageObj, callback) {
+    $.ajax({
+        url: "https://api.projectoxford.ai/emotion/v1.0/recognize",
+        beforeSend: function (xhrObj) {
+            // Request headers
+            xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "d342c8d19d4e4aafbf64ed9f025aecc8");
+        },
+        type: "POST",
+        data: messageObj,
+        processData: false
+    })
+        .done(function (data) {
+        if (data.length != 0) {
+            // Get the emotion scores
+            var scores = data[0].scores;
+            callback(scores);
+        }
+        else {
+            pageheader.innerHTML = "Hmm, we can't detect a human face in that photo. Try another?";
+        }
+    })
+        .fail(function (error) {
+        pageheader.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
+        console.log(error.getAllResponseHeaders());
+    });
+}
 
+
+
+
+    var ivona = new Ivona({
+        accessKey: 'GDNAIPKHQUXJOXRGVIFQ',
+        secretKey: 'xO9i/w4YVBdu62Vuh2B+u0ku6SnoDoBb4eHJHTzT'
+    });
+
+    ivona.listVoices()
+        .on('complete', function(voices) {
+            console.log(voices);
+        });
+
+    //  ivona.createVoice(text, config)
+    //  [string] text - the text to be spoken
+    //  [object] config (optional) - override Ivona request via 'body' value
+    ivona.createVoice('Testing for sound.' + messageObj, {
+        body: {
+            voice: {
+                name: 'Salli',
+                language: 'en-US',
+                gender: 'Female'
+            }
+        }
+    }).pipe(fs.createWriteStream('text.mp3'));
 
 
 }
