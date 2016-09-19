5af28e3485272b4b0422ecec542af556eb272f16
MichaelDB1 <michaelbarr@hotmail.co.nz>
2016-09-19 12:25:21 +1200

    more stuff

--- js/mainScript.js
+++ js/mainScript.js
@@ -4,7 +4,18 @@ var output = $("#output")[0];
 var messageObj;
 var translation;
 var messageform;
+var character;
+var URL;
+var characterSelection;
+var myTranslator;
  
+
+
+$('#characterSelection').change(function () {
+    var characterSelection = $(this).find("option:selected").text();
+    console.log("changed to " + characterSelection);  
+});
+
 
 
 function updateUI(obj) {
@@ -13,29 +24,80 @@ function updateUI(obj) {
 };
 
 
+
+
+
+function getCharacter(characterSelection, callback) {
+        
+    if (characterSelection == "Pirate") {
+        charcter ="Pirate";
+        URL = "http://api.funtranslations.com/translate/pirate.json";
+        }  
+    else if (characterSelection == "Minion") {
+        character = "Minion";
+        URL = "http://api.funtranslations.com/translate/minion.json";
+        }  
+    else if (characterSelection == "Dothraki") {
+        character = "Dothraki";
+        URL = "http://api.funtranslations.com/translate/dothraki.json";
+        }  
+    else if (characterSelection == "Gungan") {
+        character = "Gungan";
+        URL = "http://api.funtranslations.com/translate/gungan.json";
+        }  
+    else if (characterSelection == "Shakespeare") {
+        character = "Shakespeare";
+        URL = "http://api.funtranslations.com/translate/shakespeare.json";
+        }      
+     else {
+        character = "Yoda";
+        }
+    callback(character, URL);
+        };
+
+
+
+
 tbtn.on("click", function () {
 
-    var message = textareabox.value; 
-    var messageform = new FormData();
-    messageform.append("text", "randomcrap");
+    var message = textareabox.value;
 
-    translateText(message, function (textconversion) {
+
+    //Only detects after the Translate button has been clicked once, need to alter this
+    $('#characterSelection').change(function (theSelection) {
+    var characterSelection = $(this).find("option:selected").text();
+    console.log("changed to " + characterSelection);
+    
+
+        getCharacter(characterSelection, function(character, URL){
+        console.log("is this fuction running")
+        myTranslator = character;
+        myURL = URL;
+        console.log(myTranslator);
+        console.log(myURL);
+    })
+     })
+
+
+
+    if (myTranslator == "Pirate" || myTranslator == "Minion" || myTranslator == "Dothraki" || myTranslator == "Gungan" || myTranslator== "Shakespeare"){
+    translateText(message, URL, function (textconversion) {       
         messageObj = textconversion;
         console.log (translation);
         console.log (messageObj);
         updateUI(messageObj);
     });
+    }
+    else {
+    yodaTranslateText(message, function (textconversion) {       
+        messageObj = textconversion;
+        console.log (messageObj);
+        updateUI(messageObj);
+    });
+    }
 });
 
-
-
-
-
-
-
-
-
-function translateText(message, callback) {
+function translateText(message, URL, callback) {
     $.ajax({
        url: "http://api.funtranslations.com/translate/pirate.json",
      // beforeSend: function (xhrObj) {
@@ -46,7 +108,7 @@ function translateText(message, callback) {
         processData: false
     }).done(function (data) {
         if (data.length != 0) {
-            alert(data.contents.translated);
+            //alert(data.contents.translated);
             var translation = data.contents.translated;
             console.log (translation);      
             callback(translation);
@@ -56,17 +118,12 @@ function translateText(message, callback) {
         }
     })
     .fail(function (error) {
-        alert("Data hasn't come in!, perhaps just try using the yoda translation as I don't have keys for the others as that would cost me too much");
+        alert("Data hasn't come in!, perhaps just try using the yoda translation as I don't have keys for the others as that would cost me too much. Yoda will default on error");
         console.log(error.getAllResponseHeaders());
     })
 }
 
-
-
-/*
-
-
-function translateText(message, callback) {
+function yodaTranslateText(message, callback) {
     $.ajax({
         url: "http://api.funtranslations.com/translate/yoda.json",
        beforeSend: function (xhrObj) {
@@ -77,9 +134,9 @@ function translateText(message, callback) {
         processData: false
     }).done(function (data) {
         if (data.length != 0) {
-            alert(data.contents.translated);
+          //  alert(data.contents.translated);
             var translation = data.contents.translated;
-            console.log (translation);      
+            //console.log (translation);      
             callback(translation);
         }
         else {
@@ -93,5 +150,3 @@ function translateText(message, callback) {
 }
 
 
-
-*/
